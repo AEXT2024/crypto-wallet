6dcbb805acb91da8f3abc01249549d9afa7df7fb
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2024-03-05 15:00:08 +0000

    feature: add WalletBalance Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
+import { WalletBalance } from './Balance/WalletBalance';
+
 export const App = () => {
   return (
     <div>
+      <WalletBalance />
     </div>
   );
 };
---
+++ src/components/Balance/WalletBalance.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Web3 from 'web3';
+import contractABI from '../../contracts/wallet.json';
+
+const contractAddress = '0xA5Cc76EdabD9511a46708490Ef432E64746B89C9';
+
+const web3 = new Web3(window.ethereum);
+const myContract = new web3.eth.Contract(contractABI.abi, contractAddress);
+
+export const WalletBalance = () => {
+  async function checkBalance() {
+    const balance = await myContract.methods.Check_Balance().call();
+    console.log(`Balance: ${balance}`);
+  }
+  return (
+    <div>
+      <h2>Wallet Balance</h2>
+      <button onClick={checkBalance}>Check Balance</button>
+      <p id="balance">Balance: 0 ETH</p>
+    </div>
+  );
+};
